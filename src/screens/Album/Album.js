144d3946530af7fe9d80dee13cfcebebf6127b6a
CarmelaMartinezMori <cmartinezmori@udesa.edu.ar>
2023-09-10 16:17:39 -0300

    album

---
+++ src/screens/Album/Album.js
@@ -0,0 +1,102 @@
+import React, { Component } from 'react';
+import './album.css'
+
+class Album extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: this.props.match.params.id,
+      info: null,
+      isFavorite: false,
+    };
+  }
+
+  componentDidMount() {
+    this.fetchAlbumData();
+    this.checkFavorites();
+  }
+
+  fetchAlbumData() {
+    fetch(`https://thingproxy.freeboard.io/fetch/https://api.deezer.com/album/${this.state.id}`)
+      .then((response) => response.json())
+      .then((data) => this.setState({ info: data }))
+      .catch((error) => console.log('Error: ' + error));
+  }
+
+  checkFavorites() {
+    let storage = localStorage.getItem('favoriteAlbums');
+    let storageToArray = JSON.parse(storage);
+
+    if (storageToArray !== null) {
+      let isAlbumInArray = storageToArray.includes(this.state.id);
+      if (isAlbumInArray) {
+        this.setState({
+          isFavorite: true,
+        });
+      }
+    }
+  }
+
+  addToFavorites(id) {
+    let storage = localStorage.getItem('favoriteAlbums');
+
+    if (!this.state.isFavorite) {
+      if (storage === null) {
+        let idInArray = [id];
+        let arrayToString = JSON.stringify(idInArray);
+        localStorage.setItem('favoriteAlbums', arrayToString);
+      } else {
+        let fromStringToArray = JSON.parse(storage);
+        fromStringToArray.push(id);
+        let arrayToString = JSON.stringify(fromStringToArray);
+        localStorage.setItem('favoriteAlbums', arrayToString);
+      }
+
+      this.setState({
+        isFavorite: true,
+      });
+    } else {
+      let storageToArray = JSON.parse(storage);
+      let filteredArray = storageToArray.filter((elm) => elm !== id);
+      let filteredToString = JSON.stringify(filteredArray);
+      localStorage.setItem('favoriteAlbums', filteredToString);
+
+      this.setState({
+        isFavorite: false,
+      });
+    }
+  }
+
+  render() {
+    const { info, isFavorite } = this.state;
+
+    return (
+      <div className='container'>
+        {info ? (
+          <div className="spacer">
+            <img className='imagen' src={info.cover_medium} alt={info.title} />
+            <h2 className='title'>{info.title}</h2>
+            <h3 className='title'>{info.artist.name}</h3>
+            <h4 className='title'>{info.genres.data[0].name}</h4>
+            <h5 className='title'>Release Date: {info.release_date}</h5>
+            <ul className='lista'>
+              {info.tracks.data.map((track) => (
+                <li key={track.id}>{track.title}</li>
+              ))}
+            </ul>
+            <button
+              className='boton'
+              onClick={() => this.addToFavorites(info.id)}
+            >
+              {isFavorite ? 'Remove from Favorites' : 'Add to Favorites'}
+            </button>
+          </div>
+        ) : (
+          <h4>Loading...</h4>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Album;
